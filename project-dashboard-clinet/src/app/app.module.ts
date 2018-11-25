d39d8699e2c6b62933ea7b6159f30a5cdaba6ba2
Eugene Murray <eugene.murray@gmail.com>
2018-11-25 17:44:25 +0000

    add initial files

---
+++ project-dashboard-clinet/src/app/app.module.ts
@@ -0,0 +1,66 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GridsterModule } from 'angular-gridster2';
+
+import {
+  MatButtonModule,
+  MatToolbarModule,
+  MatDialogModule,
+  MatTableModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatSelectModule
+} from '@angular/material';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactUsComponent } from './home/contact-us.component';
+import { ProjectListComponent } from './projects/project-list.component';
+import { ProjectComponent } from './projects/project.component';
+import { AddEditMilestoneDialogComponent } from './projects/add-edit-milestone-dialog.component';
+import { AdminModule } from './admin/admin.module';
+import { CoreModule } from './core/core.module';
+import { UnauthorizedComponent } from './home/unauthorized.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ContactUsComponent,
+    ProjectListComponent,
+    ProjectComponent,
+    AddEditMilestoneDialogComponent,
+    UnauthorizedComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatDialogModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    AdminModule,
+    CoreModule,
+    AppRoutingModule,
+    GridsterModule 
+  ],
+  providers: [
+  ],
+  bootstrap: [AppComponent],
+  entryComponents:[
+    AddEditMilestoneDialogComponent
+  ]
+})
+export class AppModule { }
